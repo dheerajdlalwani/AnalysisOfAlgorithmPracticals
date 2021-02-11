4536bae21582452a35d78d24990748e9fab3e97c
Dheeraj Lalwani <lalwanidheeraj1234@gmail.com>
2021-02-11 18:48:33 +0530

    :sparkles: chore: add insertion sort code

---
+++ insertion_sort.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#define MAX 100
+int data[MAX], n, i;
+
+void printArray(int array[])
+{
+    // int n = 10;
+    for (i = 0; i < n; i++)
+    {
+        printf("| %d |", array[i]);
+    }
+    printf("\n");
+}
+
+void insertionSort(int array[])
+{
+    int i, j, temp;
+    for (i = 1; i < n; i++)
+    {
+        temp = array[i];
+        j = i - 1;
+        while (j >= 0 && array[j] > temp)
+        {
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = temp;
+    }
+}
+
+int main()
+{
+    printf("\n");
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Start entering %d integers: \n", n);
+    for (i = 0; i < n; i++)
+    {
+        printf("data[%d] = ", i);
+        scanf("%d", &data[i]);
+    }
+
+    printf("------- Array before sorting -------\n");
+    printArray(data);
+    printf("------- x ------- x ------- x -------\n");
+
+    insertionSort(data);
+
+    printf("------- Array after sorting -------\n");
+    printArray(data);
+    printf("------- x ------- x ------- x -------\n");
+    printf("\n");
+
+    return 0;
+}
